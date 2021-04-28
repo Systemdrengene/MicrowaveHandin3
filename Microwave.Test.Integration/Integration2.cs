0544056731e7f40a0a248ee3501c6ca66a000c32
SimonPhiDang <201705957@post.au.dk>
2021-04-28 10:12:39 +0200

    Updatet nogle værdier til milisekunder i test

--- Microwave.Test.Integration/Integration2.cs
+++ Microwave.Test.Integration/Integration2.cs
@@ -44,7 +44,7 @@ namespace Microwave.Test.Integration
             _sut.StartCooking(20,4000);
             Thread.Sleep(1000);
             _sut.Stop();
-            Assert.That(_timer.TimeRemaining, Is.EqualTo(4000));
+            Assert.That(_timer.TimeRemaining, Is.EqualTo(3000));
             
             //Assert.AreEqual(_timer.Enabled, true);
             //Assert.AreEqual(_timer.Timer, time);
@@ -53,7 +53,7 @@ namespace Microwave.Test.Integration
         [Test]
         public void StartCookingTwoSeconds_WaitTwoSeconds_TimerRemainingIsZero()
         {
-            _sut.StartCooking(20,2);
+            _sut.StartCooking(20,2000);
             Thread.Sleep(2100);
             Assert.That(()=> _timer.TimeRemaining == 0);
         }
@@ -61,7 +61,7 @@ namespace Microwave.Test.Integration
         [Test]
         public void StartCookingOneSeconds_WaitForStopping_EventsAreSend()
         {
-            _sut.StartCooking(20,1);
+            _sut.StartCooking(20,1000);
 
 	        Thread.Sleep(1100); //Vent til timer slut 100 ms over
 
