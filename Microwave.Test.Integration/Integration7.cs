24bc9146140ced9a428050f55fc3d5b545496b5b
rasmoller <rasmus28.moller@gmail.com>
2021-04-29 14:12:44 +0200

    Initial create of integration step 7

--- Microwave.Test.Integration/Integration7.cs
+++ Microwave.Test.Integration/Integration7.cs
@@ -1,3 +1,7 @@
+using Microwave.Classes.Boundary;
+using Microwave.Classes.Controllers;
+using Microwave.Classes.Interfaces;
+using NSubstitute;
 using NUnit.Framework;
 
 /*
@@ -10,9 +14,37 @@ namespace Microwave.Test.Integration
 {
     public class IT7_FullIntegration
     {
+        private UserInterface userI;
+
+        private CookController CookCtrl;
+        private IDisplay display;
+        private IDoor door;
+        private IPowerTube PT;
+        private IOutput output;
+
+        private IButton pwrBtn, timBtn, strtCnlBtn;
+        private ILight light;
+        private ITimer timer;
+
         [SetUp]
         public void Setup()
         {
+            // Integrated
+            pwrBtn = new Button();
+            timBtn = new Button();
+            strtCnlBtn = new Button();
+            output = new Output();
+            light = new Light(output);
+            timer = new Timer();
+            door = new Door();
+            display = new Display(output);
+            PT = new PowerTube(output);
+            CookCtrl = new CookController(timer, display, PT);
+            // Test
+            userI = new UserInterface(pwrBtn, timBtn, strtCnlBtn, door, display, light, CookCtrl);
+
+            CookCtrl.UI = userI;
+
         }
 
         [Test]
