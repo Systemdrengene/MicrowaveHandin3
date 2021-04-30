72f8328ad52fc1e67f035851345db96ea9663808
SimonPhiDang <201705957@post.au.dk>
2021-04-30 13:18:20 +0200

    Changed IntegrationTest2 from ms to s

--- Microwave.Test.Integration/Integration2.cs
+++ Microwave.Test.Integration/Integration2.cs
@@ -43,16 +43,16 @@ namespace Microwave.Test.Integration
         {
 
 
-            _sut.StartCooking(20,4000);
+            _sut.StartCooking(20,4);
             Thread.Sleep(1300);
             _sut.Stop();
-            Assert.That(_timer.TimeRemaining, Is.EqualTo(3000));
+            Assert.That(_timer.TimeRemaining, Is.EqualTo(3));
         }
 
         [Test]
         public void StartCookingTwoSeconds_WaitTwoSeconds_TimerRemainingIsZero()
         {
-            _sut.StartCooking(20,2000);
+            _sut.StartCooking(20,2);
             Thread.Sleep(2300);
             Assert.That(()=> _timer.TimeRemaining == 0);
         }
@@ -60,7 +60,7 @@ namespace Microwave.Test.Integration
         [Test]
         public void StartCookingOneSeconds_WaitForStopping_EventsAreSend()
         {
-            _sut.StartCooking(20,1000);
+            _sut.StartCooking(20,1);
 
 	        Thread.Sleep(1400); //Vent til timer slut 100 ms over
 
@@ -70,7 +70,7 @@ namespace Microwave.Test.Integration
         [Test]
         public void StartCookingTwoSeconds_DisplayShowTime()
         {
-            _sut.StartCooking(20,2000);
+            _sut.StartCooking(20,2);
             
             Thread.Sleep(2300);
             
